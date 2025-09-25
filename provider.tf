d92754545bda92b31f22da25a54edc4aca87ad07
Ganesh <ganeshmano1042@gmail.com>
2025-09-25 11:48:32 +0530

    final commit

---
+++ provider.tf
@@ -0,0 +1,16 @@
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
